988d5f6a47c20c111e9eebe12f0cfe6bcd7e1287
Pepeoawvh <jpp.valdes@gmail.com>
2025-01-10 00:26:48 -0300

    boton preguntas

--- src/app/components/client/botonPreguntas.jsx
+++ src/app/components/client/botonPreguntas.jsx
@@ -8,9 +8,9 @@ const BotonPreguntas = () => {
     <div className={`${bebas.className} grid grid-cols-3 w-full items-center justify-items-center h-auto text-4xl bg-gradient-to-r from-[#2694e7] to-[#96c0e1] p-4 shadow-md mx-4`}>
       <span className='col-span-2 h-fit items-center justify-items-center text-center'>¿Tienes dudas?</span> 
       <Link href="/faq" passHref>
-        <div className='col-span-1 mr-6 animation-jump'>
+        <div className='mr-6'>
           <button
-            className={`${styles.zoomImage} bg-[#2694e7] shadow-md justify-self-center border-4 border-white py-2 px-3 rounded-full text-xl pt-3 text-white`}
+            className={`${styles.zoomImage} bg-[#2694e7] shadow-md justify-self-center border-2 border-white py-2 px-2 rounded-full text-base text-white`}
           >
             Preguntas Frecuentes
           </button>
